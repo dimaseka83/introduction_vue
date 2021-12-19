c346cc61c67391332cd063ab3e218678b4d3b299
dhimas <dimaseka83@gmail.com>
2021-12-19 13:38:13 +0700

    push

---
+++ main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+import { debounce } from 'lodash-es'
+
+console.log(debounce);
+
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
