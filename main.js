f6358b526d6f117ea21d93a16a5f7efc53c154e1
dhimas <dimaseka83@gmail.com>
2021-12-21 21:06:21 +0700

    push

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Socks is out of stock'
+            image: './assets/images/socks_green.jpg',
+            url: 'https://google.com'
         }
     }
 }) 
