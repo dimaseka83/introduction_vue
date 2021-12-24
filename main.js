348bea36ce2d92b4af3eb2556afd7f7d7ed62374
dhimas <dimaseka83@gmail.com>
2021-12-24 20:50:14 +0700

    push

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inventory: 8,
-            onSale: true
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            details: ['50% cotton', '30% wool', '20% polyster'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+            ],
+        }
+    },
+    methods: {
+        addToChart() {
+            this.cart += 1
+        },
+        updateVariant(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        onSale() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            return this.brand + ' ' + this.product + ' is on sale'
         }
     }
 }) 
